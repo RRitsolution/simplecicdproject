23cd6e88033353ad297880d64864b072d4fd8f0d
Nirmal <nirmal.elex@gmail.com>
2025-10-31 01:31:43 +0100

    1st code of application

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python
+
+WORKDIR /app
+
+COPY app.py .
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python","app.py"]
---
+++ app.py
@@ -0,0 +1,22 @@
+##import Flask class from flask package
+from flask import Flask
+
+##Start flask nirmal insatnce 
+app = Flask(__name__)
+##  It is for defining endpoint
+@app.route("/")### mail root url
+
+##defined function fo home means main url call or endpoint
+def home():
+    return "This is my 1st python code"
+
+
+@app.route("/nirmal")
+
+def nirmal():
+
+    return "This is nirmal call"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
