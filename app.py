93da0c2f41ac1aa802b5a4dae4ccce40a7d25add
Nirmal <nirmal.elex@gmail.com>
2025-11-01 17:01:44 +0100

    3rd code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 ##defined function fo home means main url call or endpoint
 def home():
-    return "This is application latest updated code -Jai kali maa Jai maa Bhawani Jai Bala ji"
+    return "This is application latest updated code -Jai kali maa Jai maa Bhawani Jai Bala ji Jaishreeram"
 
 
 @app.route("/nirmal")
