a66cb9745492eea09dd4993dc13704e35587822a
Nirmal <nirmal.elex@gmail.com>
2025-11-04 17:17:50 +0100

    updated code added for nirmal api

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 ##defined function fo home means main url call or endpoint
 def home():
-    return "Application deployment successfully done for Production env//new Features added on 4th November Jai shree ram"
+    return "Application deployment successfully done for Production env//new Features added on 4th November Jai shree ram Jai Bala Ji"
 
 
 @app.route("/nirmal")
